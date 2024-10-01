7b66e3a16907c10537e152d951325ceed6853e6d
IdutshaneAEE <john.heber.huallpa.cisneros09@gmail.com>
2024-10-01 20:53:31 -0300

    primer commit

---
+++ src/main/java/com/github/ivan/clienteservidor/Clientes/Clientes_UDP.java
@@ -0,0 +1,42 @@
+package com.github.ivan.clienteservidor.Clientes;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Clientes_UDP {
+
+  public static void main(String[] args) {
+    final int PUERTO_SERVIDOR = 5001;
+    byte[] buffer = new byte[1024];
+
+    try {
+      InetAddress direccionServidor = InetAddress.getByName("localhost");
+      DatagramSocket socketUDP = new DatagramSocket();
+
+      String mensaje = "¡Hola soy el cliente!";
+      buffer = mensaje.getBytes();
+
+      DatagramPacket envioAlServidor = new DatagramPacket(buffer, buffer.length, direccionServidor, PUERTO_SERVIDOR);
+      System.out.println("Envio data grama");
+      socketUDP.send(envioAlServidor);
+
+      DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+      socketUDP.receive(peticion);
+      System.out.println("Recibo peticion");
+      mensaje = new String(peticion.getData());
+      System.out.println(mensaje);
+
+      socketUDP.close();
+
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}
